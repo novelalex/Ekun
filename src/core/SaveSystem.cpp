d75396e775c9f99777c29a5a4c7ffd32afbde08f
lowdowntotheground <dlow0831@gmail.com>
2024-10-11 15:19:21 -0400

    saving is implemented. :) <3

--- src/core/SaveSystem.cpp
+++ src/core/SaveSystem.cpp
@@ -1,8 +1,21 @@
 #include "SaveSystem.h"
+#include <fstream>
+
+using namespace std;
 
 
 
 void SaveSystem::saveData(int data) {
+   
+    
+    
+    ofstream fout;
+    
+    fout.open("savedata.txt");
+
+    fout << data << endl;
+    
+    fout.close();
 
 }
 
